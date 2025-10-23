0fe1b8487bc91dd3d0d3bb5ed27caf9378f905a1
JisiHu <2730057537@qq.com>
2025-10-23 18:22:24 +0800

    Initial commit: New blog project without history

---
+++ js/flexsearch.zh-cn.5fa0fb3300f45c344922910918968439466067f8f91fe308e1808ac5550891ea.js
@@ -0,0 +1,1 @@
+(()=>{document.addEventListener("DOMContentLoaded",function(){window.flexsearchLanguages=window.flexsearchLanguages||{},window.flexsearchLanguages["zh-cn"]={encode:function(e){return e}}});})();
